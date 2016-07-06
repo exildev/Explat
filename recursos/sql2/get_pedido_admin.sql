c785c8d214d96e31ca48aeaf8d791c052f39769b
darkdrei <darkdrei88@gmail.com>
2016-07-06 13:52:53 -0500

    actualizacion para q la consulta incluya el tiempo de reenvio del gps para los pedidos de plataforma

--- recursos/sql2/get_pedido_admin.sql
+++ recursos/sql2/get_pedido_admin.sql
@@ -6,9 +6,11 @@ CREATE OR REPLACE FUNCTION get_add_pedido_admin(id_pedido integer)
   RETURNS json AS
 $BODY$
 declare 
+	configuracion record;
 begin
+	select c.gps from usuario_tienda as t inner join usuario_empresa as e on (t.empresa_id=e.id and t.id=2) inner join pedido_configuraciontiempo as c on (c.empresa_id=e.id) limit 1 into configuracion;
 	return (SELECT COALESCE(array_to_json(array_agg(row_to_json(p))), '[]') from (
-		select id, (select identifier from motorizado_motorizado where empleado_id=motorizado_id limit 1) as motorizado,10000*(select retraso from pedido_configuraciontiempo order by id desc limit 1) as retraso,
+		select case when configuracion.gps is not null then configuracion.gps*1000 else 100 end as tiempo_gps, id, (select identifier from motorizado_motorizado where empleado_id=motorizado_id limit 1) as motorizado,10000*(select retraso from pedido_configuraciontiempo order by id desc limit 1) as retraso,
 		(
 			SELECT COALESCE(array_to_json(array_agg(row_to_json(emp))), '[]') from (
 						select id,nit,direccion,latitud,longitud,referencia,celular,fijo from usuario_tienda where id = tienda_id limit 1
@@ -35,3 +37,4 @@ $BODY$
   COST 100;
 ALTER FUNCTION get_add_pedido_admin(integer)
   OWNER TO postgres;
+select get_add_pedido_admin(10)

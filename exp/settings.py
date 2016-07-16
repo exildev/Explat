d7e644e8962c70c196f94f041f644681469fd14a
Yamid Pico <ypicoleal@users.noreply.github.com>
2016-07-16 18:06:01 -0500

    cambio de base de datos

--- exp/settings.py
+++ exp/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         # Or path to database file if using sqlite3.
-        'NAME': 'exp',
+        'NAME': 'express_norte',
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
         'PASSWORD': 'Exile*74522547',

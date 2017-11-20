62dacac3882befcfdeb74133ed4b28681d15e191
root <root@ip-172-31-46-232.us-east-2.compute.internal>
2017-11-20 00:54:17 +0000

    actualizacion depencias

--- exp/settings.py
+++ exp/settings.py
@@ -105,10 +105,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         # Or path to database file if using sqlite3.
-        'NAME': 'express',
+        'NAME': 'envios',
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'edc#$%IKP89=?',
+        'USER': 'epic',
+        'PASSWORD': '96522569*74522547',
         'HOST': '127.0.0.1',
         # Empty for localhost through domain sockets or '127.0.0.1' for
         # localhost through TCP.

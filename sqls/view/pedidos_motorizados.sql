ba6b4f957f377d2cd7e49e15fa944328759df715
darkdrei <darkdrei88@gmail.com>
2017-12-10 19:41:06 -0500

    vista de pedidos de motorizados sql

---
+++ sqls/view/pedidos_motorizados.sql
@@ -0,0 +1,27 @@
+-- View: public.pedidos_motorizados
+
+-- DROP VIEW public.pedidos_motorizados;
+
+CREATE OR REPLACE VIEW public.pedidos_motorizados AS
+ SELECT pedido.id,
+    ptime.creado,
+    ptime.confirmado,
+    ptime.despachado,
+    ptime.entregado,
+    pedido.motorizado_id
+   FROM pedido_pedido pedido
+     JOIN pedido_time ptime ON pedido.id = ptime.pedido_id
+  WHERE pedido.motorizado_id IS NOT NULL
+UNION
+ SELECT pedido.id,
+    ptime.creado,
+    ptime.confirmado,
+    ptime.despachado,
+    ptime.entregado,
+    pedido.motorizado_id
+   FROM pedido_pedidows pedido
+     JOIN pedido_timews ptime ON pedido.id = ptime.pedido_id
+  WHERE pedido.motorizado_id IS NOT NULL;
+
+ALTER TABLE public.pedidos_motorizados
+  OWNER TO postgres;

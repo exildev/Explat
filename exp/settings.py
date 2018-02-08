cd76fb542daef4c99935b22edfd4493ac5273a77
darkdrei <darkdrei88@gmail.com>
2018-02-07 22:30:58 -0500

    Actualizacion,  de dependencias

--- exp/settings.py
+++ exp/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
         'PASSWORD': '85412369**$%%&*(/%$#qazxswEDC',
-        'HOST': '127.0.0.1',
+        'HOST': 'ec2-54-175-121-21.compute-1.amazonaws.com',
         # Empty for localhost through domain sockets or '127.0.0.1' for
         # localhost through TCP.
         'PORT': '5432',  # Set to empty string for default.

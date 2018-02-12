5764e17f8d0e0756b90d8e44cd5cb287ced12baf
darkdrei <darkdrei88@gmail.com>
2018-02-12 02:03:43 -0500

    Agregar el atributo empresa y tienda

--- usuario/forms.py
+++ usuario/forms.py
@@ -197,7 +197,7 @@ class ChangePasswordForm(forms.Form):
 class SolicitudAccesoForm(forms.ModelForm):
     class Meta:
         model = models.AccesoPlataforma
-        fields = ['empresa','idenficacion','identificador', 'nombre']
+        fields = ['emp','idenficacion','identificador', 'nombre']
         exclude = ['status','autorizado']
     #end class
 #end class
@@ -206,10 +206,9 @@ class SolicitudAccesoForm(forms.ModelForm):
 class SolicitudAccesoAdminForm(forms.ModelForm):
     class Meta:
         model = models.AccesoPlataforma
-        fields = ['empresa','idenficacion','identificador', 'nombre','status','autorizado']
+        fields = ['emp','idenficacion','identificador', 'nombre','status','autorizado']
         widgets ={
             "nombre":forms.TextInput(attrs={'placeholder': 'Nombres', 'required': True}),
-            "empresa": forms.TextInput(attrs={'placeholder': 'Apellidos', 'required': True}),
             "identificacion": forms.NumberInput(attrs={'placeholder': 'Numero de Identificación'}),
             "identificador": forms.NumberInput(attrs={'placeholder': 'Numero de Identificación'}),
         }
